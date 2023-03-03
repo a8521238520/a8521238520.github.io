28171f21329e6957cc488298ba569d52bd8b14a3
a8521238520 <82221566+a8521238520@users.noreply.github.com>
2023-03-03 22:09:40 +0800

    Update script.js

--- other/script.js
+++ other/script.js
@@ -1,18 +1,11 @@
-// 設定倒數計時的目標日期和時間
-var countDownDate = new Date("Mar 5, 2023 12:00:00").getTime();
-
-// 更新倒數計時
-var x = setInterval(function() {
-    // 現在的日期和時間
-    var now = new Date().getTime();
-
-    // 距離目標日期和時間的毫秒數
-    var distance = countDownDate - now;
-
-    // 計算天、小時、分鐘和秒數
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-// 在網頁
+    var countDown = 10;
+    var countdownEl = document.getElementById("countdown");
+    var countdownInterval = setInterval(function() {
+        if (countDown > 0) {
+        countdownEl.innerHTML =  + countDown + " 秒" + " 重新載入";
+        countDown--;
+        } else {
+        clearInterval(countdownInterval);
+        countdownEl.innerHTML = "載入中";
+        }
+    }, 1000);
